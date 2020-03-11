ad1d7e9ab67bfceb582159ca3a2ebd3ff971000b
Your Name <27681135@qq.com>
2020-03-11 23:18:10 +0800

    demo

---
+++ demo1.sql
@@ -0,0 +1,30 @@
+\echo Use "CREATE EXTENSION pgjwt" to load this file. \quit
+
+
+create schema api;
+
+create table api.articles (
+  id serial primary key,
+  userid serial not null,
+  title text,
+  content text,
+  show boolean not null default true,
+  createdAt timestamptz  default current_timestamp
+);
+
+insert into api.articles (userid,title,content) values
+  (1,'第一篇文章标题','内容'), (1,'第二篇标题','内容2');
+
+select * from api.articles;
+
+create role web_anon nologin;
+
+grant usage on schema api to web_anon;
+grant select on api.articles to web_anon;
+create role authenticator noinherit login password 'mypassword';
+grant web_anon to authenticator;
+create role auth_user nologin;
+grant auth_user to authenticator;
+grant usage on schema api to auth_user;
+grant all on api.articles to auth_user;
+grant usage, select on sequence api.articles_id_seq to auth_user;
---
+++ demo2.sql
@@ -0,0 +1,114 @@
+\echo jwtDemo2 \quit
+
+CREATE EXTENSION pgjwt
+
+CREATE TYPE jwt_token AS (
+  token text
+);
+
+CREATE FUNCTION jwt_test() RETURNS public.jwt_token AS $$
+  SELECT public.sign(
+    row_to_json(r), 'reallyreallyreallyreallyverysafe'
+  ) AS token
+  FROM (
+    SELECT
+      'my_role'::text as role,
+      extract(epoch from now())::integer + 300 AS exp
+  ) r;
+$$ LANGUAGE sql;
+
+-- run this once
+ALTER DATABASE postgres SET "app.jwt_secret" TO 'reallayreallybreally1reallcyverysafe';
+
+create schema if not exists basic_auth;
+
+create table if not exists
+basic_auth.users (
+  email    text primary key check ( email ~* '^.+@.+\..+$' ),
+  pass     text not null check (length(pass) < 512),
+  role     name not null check (length(role) < 512)
+);
+
+create or replace function
+basic_auth.check_role_exists() returns trigger as $$
+begin
+  if not exists (select 1 from pg_roles as r where r.rolname = new.role) then
+    raise foreign_key_violation using message =
+      'unknown database role: ' || new.role;
+    return null;
+  end if;
+  return new;
+end
+$$ language plpgsql;
+
+drop trigger if exists ensure_user_role_exists on basic_auth.users;
+create constraint trigger ensure_user_role_exists
+  after insert or update on basic_auth.users
+  for each row
+  execute procedure basic_auth.check_role_exists();
+
+-- 安装pgcrypto插件并设置触发，在新建用户时加密密码信息。
+create extension if not exists pgcrypto;
+
+create or replace function
+basic_auth.encrypt_pass() returns trigger as $$
+begin
+  if tg_op = 'INSERT' or new.pass <> old.pass then
+    new.pass = crypt(new.pass, gen_salt('bf'));
+  end if;
+  return new;
+end
+$$ language plpgsql;
+
+drop trigger if exists encrypt_pass on basic_auth.users;
+create trigger encrypt_pass
+  before insert or update on basic_auth.users
+  for each row
+  execute procedure basic_auth.encrypt_pass();
+
+-- 创建比对函数，用户密码验证通过则返回角色
+create or replace function
+basic_auth.user_role(email text, pass text) returns name
+  language plpgsql
+  as $$
+begin
+  return (
+  select role from basic_auth.users
+   where users.email = user_role.email
+     and users.pass = crypt(user_role.pass, users.pass)
+  );
+end;
+$$;
+
+
+-- login should be on your exposed schema
+create or replace function
+login(email text, pass text) returns basic_auth.jwt_token as $$
+declare
+  _role name;
+  result basic_auth.jwt_token;
+begin
+  -- check email and password
+  select basic_auth.user_role(email, pass) into _role;
+  if _role is null then
+    raise invalid_password using message = 'invalid user or password';
+  end if;
+
+  select sign(
+      row_to_json(r), current_setting('app.jwt_secret')
+    ) as token
+    from (
+      select _role as role, login.email as email,
+         extract(epoch from now())::integer + 60*60 as exp
+    ) r
+    into result;
+  return result;
+end;
+$$ language plpgsql security definer;
+
+
+
+
+
+
+
